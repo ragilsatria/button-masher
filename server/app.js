a515fcfcf666c90e3ec4d0272422a4921d4b57eb
Faturachman Yusup <faturachmany@gmail.com>
2020-07-24 00:29:06 +0700

    menambahkan css pada halaman awal

--- server/app.js
+++ server/app.js
@@ -3,9 +3,15 @@ const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const PORT = 3000
+const cors = require('cors');
 
+
+app.use(cors());
 io.on('connection', (socket) => {
-
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
 })
 
 http.listen(PORT, () => {
